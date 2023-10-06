d8bb60c12386a7a8951e1deced56c46e949fb537
Noel Bryant <noelsobryant@gmail.com>
2023-10-06 16:16:26 +0300

    add routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Categories from './components/Categories';
+import Header from './components/Header';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Bookstore</h1>
-    </div>
-  );
-}
+const App = () => (
+  <>
+    <Header />
+    <Home />
+  </>
+);
+
+<BrowserRouter>
+  <Routes>
+    <Route path="/" element={<Home />}>
+      <Route index element={<Home />} />
+    </Route>
+    <Route path="/categories" element={<Categories />} />
+  </Routes>
+</BrowserRouter>;
 
 export default App;
