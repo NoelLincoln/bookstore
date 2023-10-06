d19c4fa1afa263d9e419557ff26f2a9c7ec08b34
Noel Bryant <noelsobryant@gmail.com>
2023-10-07 00:13:07 +0300

    add form for adding book

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -13,27 +13,30 @@ const AddBook = ({ onAddBook }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        placeholder="Title"
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-      />
+    <>
+      <h3>Add New Book</h3>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Book Title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
 
-      <select
-        id="categoryDropdown"
-        value={category}
-        onChange={(e) => setCategory(e.target.value)}
-      >
-        <option value="">Category</option>
-        <option value="Author 1">React</option>
-        <option value="Author 2">Data structures</option>
-        <option value="Author 3">Ruby</option>
-      </select>
+        <select
+          id="categoryDropdown"
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
+        >
+          <option value="">Category</option>
+          <option value="Author 1">React</option>
+          <option value="Author 2">Data structures</option>
+          <option value="Author 3">Ruby</option>
+        </select>
 
-      <button type="submit">Add Book</button>
-    </form>
+        <button type="submit">Add Book</button>
+      </form>
+    </>
   );
 };
 
--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,12 +1,14 @@
 import { PropTypes } from 'prop-types';
 import React from 'react';
 import BookItem from './BookItem';
+import AddBook from './AddBook';
 
 const BookList = ({ books, onDelete }) => (
   <div className="book-list">
     {books.map((book) => (
       <BookItem key={book.id} book={book} onDelete={() => onDelete(book.id)} />
     ))}
+    <AddBook />
   </div>
 );
 
@@ -18,7 +20,7 @@ BookList.propTypes = {
       title: PropTypes.string.isRequired,
       author: PropTypes.string.isRequired,
       progress: PropTypes.string.isRequired,
-    }),
+    })
   ).isRequired,
   onDelete: PropTypes.func.isRequired, // Assuming onDelete is a function that removes a book by ID
 };
