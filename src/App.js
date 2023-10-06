788916b1293fcbf7105486cbfed60afbdd0c17b4
Noel Bryant <noelsobryant@gmail.com>
2023-10-06 22:53:40 +0300

    set initial state for App and pass down state via props to children

--- src/App.js
+++ src/App.js
@@ -1,20 +1,50 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './components/Home';
+import { useState } from 'react';
 import Categories from './components/Categories';
 import Header from './components/Header';
+import BookList from './components/BookList';
+import AddBooK from './components/AddBook';
 import './App.css';
 
-const App = () => (
-  <>
-    <Header />
-    <Home />
-  </>
-);
+const App = () => {
+  const [books, setBooks] = useState([
+    {
+      id: 1,
+      title: 'Noel',
+      author: 'Noel',
+      genre: 'Data Science',
+      progress: '0%',
+    },
+    {
+      id: 2,
+      title: 'Bryant',
+      author: 'Bryant',
+      genre: 'React',
+      progress: '0%',
+    },
+  ]);
+  const addBook = (newBook) => {
+    const updatedBooks = [...books, newBook];
+    setBooks(updatedBooks);
+  };
+
+  const onDelete = (bookId) => {
+    setBooks((prevBooks) => prevBooks.filter((book) => book.id !== bookId));
+  };
+
+  return (
+    <>
+      <Header />
+      <BookList books={books} onDelete={onDelete} />
+      <AddBooK onAddBook={addBook} />
+    </>
+  );
+};
 
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Home />}>
-        <Route index element={<Home />} />
+      <Route path="/" element={<BookList />}>
+        <Route index element={<BookList />} />
       </Route>
       <Route path="/categories" element={<Categories />} />
     </Routes>
