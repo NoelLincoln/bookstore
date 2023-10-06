32c55f423e997dd6e44068fb333e2ccfa180f1f3
Noel Bryant <noelsobryant@gmail.com>
2023-10-06 23:31:34 +0300

    fix linter errors

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { useState } from 'react';
 import Categories from './components/Categories';
 import Header from './components/Header';
