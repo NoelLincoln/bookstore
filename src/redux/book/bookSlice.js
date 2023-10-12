7c01262c8aecfe51171dbcebe5809b6115c51a96
Noel Bryant <noelsobryant@gmail.com>
2023-10-12 04:05:23 +0300

    set initial book state and fix remove book action to correctly remove the clicked item id

--- src/redux/book/bookSlice.js
+++ src/redux/book/bookSlice.js
@@ -1,7 +1,26 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  books: [],
+  books: [
+    {
+      itemId: 'item1',
+      title: 'The Great Gatsby',
+      author: 'John Smith',
+      category: 'Fiction',
+    },
+    {
+      itemId: 'item2',
+      title: 'Anna Karenina',
+      author: 'Leo Tolstoy',
+      category: 'Fiction',
+    },
+    {
+      itemId: 'item3',
+      title: 'The Selfish Gene',
+      author: 'Richard Dawkins',
+      category: 'Nonfiction',
+    },
+  ],
 };
 
 const bookSlice = createSlice({
@@ -12,7 +31,8 @@ const bookSlice = createSlice({
       state.books.push(action.payload);
     },
     removeBook: (state, action) => {
-      state.books = state.books.filter((book) => book.id !== action.payload.id);
+      const itemId = action.payload;
+      state.books = state.books.filter((book) => book.itemId !== itemId);
     },
   },
 });
