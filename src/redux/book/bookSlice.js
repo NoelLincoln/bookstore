8046d41b8d783b26f898b8c2ffeb537906470f23
Noel Bryant <noelsobryant@gmail.com>
2023-10-13 22:11:56 +0300

    set initial state of book to empty array

--- src/redux/book/bookSlice.js
+++ src/redux/book/bookSlice.js
@@ -1,26 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  books: [
-    {
-      itemId: 'item1',
-      title: 'The Great Gatsby',
-      author: 'John Smith',
-      category: 'Fiction',
-    },
-    {
-      itemId: 'item2',
-      title: 'Anna Karenina',
-      author: 'Leo Tolstoy',
-      category: 'Fiction',
-    },
-    {
-      itemId: 'item3',
-      title: 'The Selfish Gene',
-      author: 'Richard Dawkins',
-      category: 'Nonfiction',
-    },
-  ],
+  books: [],
 };
 
 const bookSlice = createSlice({
