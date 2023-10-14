a608679bb34a1d1c57db6c6abb6117085346fefb
Noel Bryant <noelsobryant@gmail.com>
2023-10-14 15:01:53 +0300

    pass down handleremoveItem and Item to children components

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,13 +1,25 @@
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchBooks, removeBookAsync, addBook } from '../redux/book/bookSlice';
 import BookList from './BookList';
 import AddBook from './AddBook';
 
 const Books = () => {
-  const books = useSelector((store) => store.books.books);
+  const dispatch = useDispatch();
+  const booksProp = useSelector((state) => state.books.books);
+
+  useEffect(() => {
+    dispatch(fetchBooks());
+  }, [dispatch]);
+
+  const handleRemoveItem = (bookId) => {
+    dispatch(removeBookAsync(bookId));
+  };
+
   return (
     <div>
-      <BookList books={books} />
-      <AddBook />
+      <BookList booksProp={booksProp} handleRemoveItem={handleRemoveItem} />
+      <AddBook addBook={addBook} />
     </div>
   );
 };
